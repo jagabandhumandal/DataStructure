a8dc83e4a4d2bd40229f9b0974cc890c42db6731
abhimanju <jagabandhu_07@yahoo.com>
2017-12-28 02:47:16 +1100

    bubble algorithm

---
+++ BubbleSort.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureAlgorithm
+{
+    public class BubbleSort
+    {
+        /* Bubble sort follows a simple logic. It compares adjacent elements in a loop and swaps them if they are not in order.
+ 
+          Bubble sort is named this way because, in this sorting method, the smaller elements gradually bubble up to the top of the list.
+ 
+          Bubble sort has worst-case and average complexity both О(n2), where n is the number of items being sorted.*/
+
+        public int[] BubbleSortAlgo(int[] numbers, int size)
+        {
+            int temp;
+            for (int i = 0; i <= size - 2; i++)
+            {
+                for (int j = 0; j <= size - 2; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        temp = numbers[j + 1];
+                        numbers[j + 1] = numbers[j];
+                        numbers[j] = temp;
+                    }
+                }
+            }
+            return numbers;
+        }
+    }
+}
---
+++ DataAlgoMain.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureAlgorithm
+{
+    class DataAlgoMain
+    {
+        static void Main(string[] args)
+        {
+            int size;
+            Console.Write("Enter the size:");
+            size = Convert.ToInt32(Console.ReadLine());
+            int[] numbers = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Before Sorting-");
+            foreach (var num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+            BubbleSort bubble = new BubbleSort();
+            int[] result = bubble.BubbleSortAlgo(numbers, size);
+            Console.WriteLine("After Sorting-");
+            foreach (var num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+            Console.ReadKey();
+        }
+
+        
+    }
+    
+}
